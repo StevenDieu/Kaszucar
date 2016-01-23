6427820e6764313ce9515e2194d9d7edabd0cba3
Steven Dieu <groover.Dieu@gmail.com>
2016-01-24 00:19:28 +0100

    Initialisation du projet
    Création de l'arborescence pour le projet en Maven + Spring MVC

---
+++ kaszucarProject/src/main/java/controller/Index.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index {
+	
+	@RequestMapping(value="/")
+    public String guestbook(HttpServletRequest request) {
+        return "index";
+    }
+
+}
